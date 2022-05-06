f88cf6eb8c7665805f23dcd2f3951c8daf9ca601
naomyte <49845989+naomyte@users.noreply.github.com>
2022-05-06 11:39:12 -0700

    Fixed the program not starting on the correct fragment.

--- app/src/main/java/edu/uw/tcss450/labose/signinandregistration/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/labose/signinandregistration/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.uw.tcss450.labose.signinandregistration;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
@@ -9,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController.navigate(R.id.fragment_sign);
         setContentView(R.layout.activity_main);
     }
 }
