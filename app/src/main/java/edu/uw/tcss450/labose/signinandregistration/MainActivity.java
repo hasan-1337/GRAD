b7090d775165074e8e2cc329ba89c2bc5dd5b8b1
naomyte <49845989+naomyte@users.noreply.github.com>
2022-05-08 13:01:08 -0700

    This time I actually fixed the issue properly. MainActivity refers to the auth_graph instead of a main_graph since we don't have one. Now we can actually access any of the java code we've written inside of the emulator.

--- app/src/main/java/edu/uw/tcss450/labose/signinandregistration/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/labose/signinandregistration/MainActivity.java
@@ -11,8 +11,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        navController.navigate(R.id.SignInFragment);
         setContentView(R.layout.activity_main);
     }
 }
