9d573f98a9a191b0de2a2b40dc8c64ec4437bd90
naomyte <49845989+naomyte@users.noreply.github.com>
2022-05-08 20:47:30 -0700

    Uncommented the error, we need to fix MainActivityArgs.

---
+++ app/src/main/java/edu/uw/tcss450/labose/signinandregistration/AuthActivity.java
@@ -0,0 +1,14 @@
+package edu.uw.tcss450.labose.signinandregistration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
