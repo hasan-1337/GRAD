9a9e42bf32298bde24f74ff5d56f8d22bb1900e7
Lidiya <labose@uw.edu>
2022-05-09 14:52:29 -0700

    [Put a commit message here for what you changed]

---
+++ Downloads/Team-2-TCSS-450-LidiyasCommit/Team-2-TCSS-450-d228f382141e31dfce808af0167fcd1d65d5ce55/app/src/main/java/edu/uw/tcss450/labose/signinandregistration/model/userViewModel.java
@@ -0,0 +1,46 @@
+package edu.uw.tcss450.labose.signinandregistration.model;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import com.auth0.android.jwt.JWT;
+public class userViewModel extends ViewModel {
+    private final String mEmail;
+    private final String mJwt;
+
+    private userViewModel(String email, String jwt) {
+        mEmail = email;
+        mJwt = jwt;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public String getmJwt() {
+        return mJwt;
+    }
+
+    public static class userViewModelFactory implements ViewModelProvider.Factory {
+
+        private final String name;
+        private final String jwt;
+
+        public userViewModelFactory(String name, String jwt) {
+            this.name = name;
+            this.jwt = jwt;
+        }
+
+        @NonNull
+        @Override
+        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+            if (modelClass == userViewModel.class) {
+                return (T) new userViewModel(name, jwt);
+            }
+            throw new IllegalArgumentException(
+                    "Argument must be: " + userViewModel.class);
+        }
+    }
+}
+
+
