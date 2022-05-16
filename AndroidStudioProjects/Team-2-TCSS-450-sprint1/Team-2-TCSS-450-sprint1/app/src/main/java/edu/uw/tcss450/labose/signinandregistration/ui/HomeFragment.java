e489ac354728e05b8dd2a7210ef938a1aed69acf
Lidiya <labose@uw.edu>
2022-05-16 15:24:55 -0700

    most of the class are created with some error

---
+++ AndroidStudioProjects/Team-2-TCSS-450-sprint1/Team-2-TCSS-450-sprint1/app/src/main/java/edu/uw/tcss450/labose/signinandregistration/ui/HomeFragment.java
@@ -0,0 +1,44 @@
+package edu.uw.tcss450.labose.signinandregistration.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+
+
+import edu.uw.tcss450.labose.signinandregistration.R;
+import edu.uw.tcss450.labose.signinandregistration.databinding.FragmentHomeBinding;
+import edu.uw.tcss450.labose.signinandregistration.model.userViewModel;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        userViewModel model = new ViewModelProvider(getActivity())
+                .get(userViewModel.class);
+
+        FragmentHomeBinding.bind(getView()).textHello.setText("Hello " + model.getEmail());
+    }
+}
+
