5a5b23cda1257cb87ebd3eeda1434f8b0f7a0d45
Hasan <hhasan1@uw.edu>
2022-05-27 21:56:50 -0700

    Implemented weather map with active input

--- app/src/main/java/edu/uw/tcss450/labose/signinandregistration/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/labose/signinandregistration/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.uw.tcss450.labose.signinandregistration;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDestination;
@@ -8,33 +10,22 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import android.annotation.SuppressLint;
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.util.Log;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.text.DecimalFormat;
-
 import edu.uw.tcss450.labose.signinandregistration.databinding.ActivityMainBinding;
+import edu.uw.tcss450.labose.signinandregistration.model.LocationViewModel;
 import edu.uw.tcss450.labose.signinandregistration.model.NewMessageCountViewModel;
 import edu.uw.tcss450.labose.signinandregistration.model.userViewModel;
 import edu.uw.tcss450.labose.signinandregistration.services.PushReceiver;
@@ -48,7 +39,23 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private MainPushMessageReceiver mPushMessageReceiver;
     private NewMessageCountViewModel mNewMessageModel;
-    private final DecimalFormat df = new DecimalFormat("#.#");
+
+    /**
+     * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+     */
+    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+    /**
+     * The fastest rate for active location updates. Exact. Updates will never be more frequent
+     * than this value.
+     */
+    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+    // A constant int for the permissions request code. Must be a 16 bit number
+    private static final int MY_PERMISSIONS_LOCATIONS = 8414;
+    private com.google.android.gms.location.LocationRequest mLocationRequest;
+    //Use a FusedLocationProviderClient to request the location
+    private FusedLocationProviderClient mFusedLocationClient;
+    //The ViewModel that will store the current location
+    private LocationViewModel mLocationModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,6 +98,21 @@ public class MainActivity extends AppCompatActivity {
                 badge.setVisible(false);
             }
         });
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION
+                            , Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_LOCATIONS);
+        } else {
+            //The user has already allowed the use of Locations. Get the current location.
+            requestLocation();
+        }
+        createLocationRequest();
     }
 
     private class MainPushMessageReceiver extends BroadcastReceiver {
@@ -141,5 +163,60 @@ public class MainActivity extends AppCompatActivity {
             unregisterReceiver(mPushMessageReceiver);
         }
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // If request is cancelled, the result arrays are empty.
+        if (requestCode == MY_PERMISSIONS_LOCATIONS) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // permission was granted, yay! Do the
+                // locations-related task you need to do.
+                requestLocation();
+            } else {
+                // permission denied, boo! Disable the
+                // functionality that depends on this permission.
+                Log.d("PERMISSION DENIED", "Nothing to see or do here.");
 
+                //Shut down the app. In production release, you would let the user
+                //know why the app is shutting down...maybe ask for permission again?
+                finishAndRemoveTask();
+            }
+        }
+    }
+    private void requestLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.d("REQUEST LOCATION", "User did NOT allow permission to request location!");
+        } else {
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    Log.d("LOCATION", location.toString());
+                    if (mLocationModel == null) {
+                        mLocationModel = new ViewModelProvider(MainActivity.this)
+                                .get(LocationViewModel.class);
+                    }
+                    mLocationModel.setLocation(location);
+                }
+            });
+        }
+    }
+    /**
+     * Create and configure a Location Request used when retrieving location updates
+     */
+    private void createLocationRequest() {
+        mLocationRequest = com.google.android.gms.location.LocationRequest.create();
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setPriority(com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
 }
--- app/src/main/java/edu/uw/tcss450/labose/signinandregistration/ui/HomeFragment.java
+++ app/src/main/java/edu/uw/tcss450/labose/signinandregistration/ui/HomeFragment.java
@@ -1,5 +1,6 @@
 package edu.uw.tcss450.labose.signinandregistration.ui;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,193 +11,242 @@ import androidx.lifecycle.ViewModelProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
-
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.gms.common.util.NumberUtils;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.DecimalFormat;
-import java.util.Locale;
 
 import edu.uw.tcss450.labose.signinandregistration.R;
 import edu.uw.tcss450.labose.signinandregistration.databinding.FragmentHomeBinding;
-import edu.uw.tcss450.labose.signinandregistration.model.userViewModel;
+import edu.uw.tcss450.labose.signinandregistration.model.LocationViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private FragmentHomeBinding mBinding;
+    private GoogleMap mMap;
     private static String hourly;
     private static String current;
     private static String daily;
+    private static Object background;
+    private static LatLng coordinates;
     private static final DecimalFormat df = new DecimalFormat("#.#");
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
+    public HomeFragment() {}
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        userViewModel model = new ViewModelProvider(getActivity())
-                .get(userViewModel.class);
 
         mBinding = FragmentHomeBinding.bind(getView());
         mBinding.hourlyWeather.setText(hourly);
         mBinding.currentWeather.setText(current);
         mBinding.tvResult.setText(daily);
         mBinding.btnGet.setOnClickListener(this::getWeatherDetails);
+
+        if (background != null) {
+            mBinding.background.setBackgroundResource((Integer) background);
+        }
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
     }
 
-    public void getWeatherDetails(View view) {
+    public void getWeatherDetails(final View view) {
         String tempUrl;
         String city = mBinding.etCity.getText().toString().trim();
+
         if(city.equals("")){ // if nothing is typed
-            mBinding.tvResult.setText("City field can not be empty!");
+            mBinding.tvResult.setText(R.string.weather_invalid);
         }else{
-            boolean bZipcode = false;
+            boolean bZipcode;
 
             try { // Check if it's a zipcode or city name
-                int d = Integer.parseInt(city);
+                Integer.parseInt(city);
                 bZipcode = true;
             } catch (final NumberFormatException nfe) {
                 bZipcode = false;
             }
 
             if (bZipcode) {
-                tempUrl = "https://api.weatherbit.io/v2.0/forecast/daily?&postal_code=" + city + "&country=US&days=7&units=I&key=51500e0f085741f591dc0356d9a03ff4";
-            } else {
-                tempUrl = "https://api.weatherbit.io/v2.0/forecast/daily?&city=" + city + "&country=US&days=7&units=I&key=51500e0f085741f591dc0356d9a03ff4";
-            }
-
-            // request the JSON information
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, response -> {
-                StringBuilder output = new StringBuilder();
-
-                try {
-                    JSONObject jsonResponse = new JSONObject(response);
-                    JSONArray jsonArray = jsonResponse.getJSONArray("data");
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        JSONObject jsonobject = jsonArray.getJSONObject(i);
-                        double temp = jsonobject.getDouble("temp");
-                        int humidity = jsonobject.getInt("rh");
-                        double wind = jsonobject.getDouble("wind_spd");
-                        String clouds = jsonobject.getString("clouds");
-                        output.append(" Day ").append(i + 1).append("\n").append(df.format(temp)).append(" °F").append("\n Humidity: ").append(humidity).append("%").append("\n Wind Speed: ").append(df.format(wind)).append(" mph").append("\n Cloudiness: ").append(clouds).append("%\n\n");
-                    }
-                    daily = output.toString();
-                    mBinding.tvResult.setText(daily); // Fill up the textview with the weather data
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }, error -> Toast.makeText(getActivity(), error.toString().trim(), Toast.LENGTH_SHORT).show());
-            RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
-            requestQueue.add(stringRequest);
-
-            if (bZipcode) {
-                tempUrl = "https://api.weatherbit.io/v2.0/current?postal_code=" + city + "&units=I&key=51500e0f085741f591dc0356d9a03ff4";
+                tempUrl = "https://api.weatherbit.io/v2.0/current?postal_code=" + city + "&country=US&units=I&key=51500e0f085741f591dc0356d9a03ff4";
             } else {
                 tempUrl = "https://api.weatherbit.io/v2.0/current?city=" + city + "&units=I&key=51500e0f085741f591dc0356d9a03ff4";
             }
-
-            // Resend the request to retrieve the current forecast.
-            stringRequest = new StringRequest(Request.Method.POST, tempUrl, response -> {
-                String output = "";
-
-                try {
-                    JSONObject jsonResponse = new JSONObject(response);
-                    JSONArray jsonArray = jsonResponse.getJSONArray("data");
-                    JSONObject jsonobject = jsonArray.getJSONObject(0);
-                    double temp = jsonobject.getDouble("temp");
-                    int humidity = jsonobject.getInt("rh");
-                    double wind = jsonobject.getDouble("wind_spd");
-                    int clouds = jsonobject.getInt("clouds");
-                    String state = jsonobject.getString("city_name");
-                    String countryCode = jsonobject.getString("country_code");
-                    output += " Current Forecast in\n" + state + " (" + countryCode + ")"
-                            + "\n" + df.format(temp) + " °F"
-                            + "\n\n Humidity: " + humidity + "%"
-                            + "\n Wind Speed: " + df.format(wind) + " mph"
-                            + "\n Cloudiness: " + clouds + "%\n\n";
-                    current = output;
-                    mBinding.currentWeather.setText(current); // Fill up the textview with the weather data
-
-                    LinearLayout layout = mBinding.background;
-                    if (temp > 35) {
-                        if (clouds > 50) {
-                            layout.setBackgroundResource(R.drawable.cloudy);
-                        } else {
-                            layout.setBackgroundResource(R.drawable.weather);
-                        }
-                    } else {
-                        layout.setBackgroundResource(R.drawable.snow);
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }, error -> Toast.makeText(getActivity(), error.toString().trim(), Toast.LENGTH_SHORT).show());
-            requestQueue = Volley.newRequestQueue(getActivity());
-            requestQueue.add(stringRequest);
+            getWeatherCurrent(tempUrl);
 
             if (bZipcode) {
-                tempUrl = "https://api.weatherbit.io/v2.0/forecast/hourly?postal_code=" + city + "&hours=24&units=I&key=51500e0f085741f591dc0356d9a03ff4";
+                tempUrl = "https://api.weatherbit.io/v2.0/forecast/hourly?postal_code=" + city + "&country=US&hours=24&units=I&key=51500e0f085741f591dc0356d9a03ff4";
             } else {
                 tempUrl = "https://api.weatherbit.io/v2.0/forecast/hourly?city=" + city + "&hours=24&units=I&key=51500e0f085741f591dc0356d9a03ff4";
             }
+            getWeatherHourly(tempUrl);
 
-            // Resend the request to retrieve the current forecast.
-            stringRequest = new StringRequest(Request.Method.POST, tempUrl, response -> {
-                StringBuilder output = new StringBuilder();
-                StringBuilder output2 = new StringBuilder();
-                try {
-                    JSONObject jsonResponse = new JSONObject(response);
-                    JSONArray jsonArray = jsonResponse.getJSONArray("data");
-                    output.append(String.format("%-4s", ""));
-
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        JSONObject jsonobject = jsonArray.getJSONObject(i);
-                        double temp = jsonobject.getDouble("temp");
-                        String time = jsonobject.getString("timestamp_local");
-                        int t = Integer.parseInt(time.substring(11, 13));
-                        String x = "" + ((t % 12 == 0)? 12:t % 12);
-                        if (t % 12 == t) {
-                            output.append(String.format("%-11s", x + " AM"));
-                        } else {
-                            output.append(String.format("%-11s", x + " PM"));
-                        }
-
-                        output2.append(df.format(temp)).append(String.format("%-8s", " °F"));
-                    }
-                    hourly = output + "\n" + output2;
-                    mBinding.hourlyWeather.setText(hourly); // Fill up the textview with the weather data
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }, error -> Toast.makeText(getActivity(), error.toString().trim(), Toast.LENGTH_SHORT).show());
-            requestQueue = Volley.newRequestQueue(getActivity());
-            requestQueue.add(stringRequest);
+            if (bZipcode) {
+                tempUrl = "https://api.weatherbit.io/v2.0/forecast/daily?&postal_code=" + city + "&country=US&days=7&units=I&key=51500e0f085741f591dc0356d9a03ff4";
+            } else {
+                tempUrl = "https://api.weatherbit.io/v2.0/forecast/daily?&city=" + city + "&days=7&units=I&key=51500e0f085741f591dc0356d9a03ff4";
+            }
+            getWeatherDaily(tempUrl);
         }
+    }
+
+    private void getWeatherCurrent(final String tempUrl) {
+        // Resend the request to retrieve the current forecast.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, response -> {
+            String output = "";
+
+            try {
+                JSONObject jsonResponse = new JSONObject(response);
+                JSONArray jsonArray = jsonResponse.getJSONArray("data");
+                JSONObject jsonobject = jsonArray.getJSONObject(0);
+                double temp = jsonobject.getDouble("temp");
+                int humidity = jsonobject.getInt("rh");
+                double wind = jsonobject.getDouble("wind_spd");
+                int clouds = jsonobject.getInt("clouds");
+                String state = jsonobject.getString("city_name");
+                String countryCode = jsonobject.getString("country_code");
+                output += " Current Forecast in\n" + state + " (" + countryCode + ")"
+                        + "\n" + df.format(temp) + " °F"
+                        + "\n\n Humidity: " + humidity + "%"
+                        + "\n Wind Speed: " + df.format(wind) + " mph"
+                        + "\n Cloudiness: " + clouds + "%\n\n";
+                current = output;
+                mBinding.currentWeather.setText(current); // Fill up the textview with the weather data
+
+                if (temp > 35) {
+                    if (clouds > 50) {
+                        background = R.drawable.cloudy;
+                    } else {
+                        background = R.drawable.weather;
+                    }
+                } else {
+                    background = R.drawable.snow;
+                }
+
+                mBinding.background.setBackgroundResource((Integer) background);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> Toast.makeText(getActivity(), error.toString().trim(), Toast.LENGTH_SHORT).show());
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        requestQueue.add(stringRequest);
+    }
+
+    private void getWeatherHourly(final String tempUrl) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, response -> {
+            StringBuilder output = new StringBuilder();
+            StringBuilder output2 = new StringBuilder();
+            try {
+                JSONObject jsonResponse = new JSONObject(response);
+                JSONArray jsonArray = jsonResponse.getJSONArray("data");
+                output.append(String.format("%-4s", ""));
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonobject = jsonArray.getJSONObject(i);
+                    double temp = jsonobject.getDouble("temp");
+                    String time = jsonobject.getString("timestamp_local");
+                    int t = Integer.parseInt(time.substring(11, 13));
+                    String x = "" + ((t % 12 == 0)? 12:t % 12);
+                    if (t % 12 == t) {
+                        output.append(String.format("%-11s", x + " AM"));
+                    } else {
+                        output.append(String.format("%-11s", x + " PM"));
+                    }
+
+                    output2.append(df.format(temp)).append(String.format("%-8s", " °F"));
+                }
+                hourly = output + "\n" + output2;
+                mBinding.hourlyWeather.setText(hourly); // Fill up the textview with the weather data
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> Toast.makeText(getActivity(), error.toString().trim(), Toast.LENGTH_SHORT).show());
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        requestQueue.add(stringRequest);
+    }
+
+    private void getWeatherDaily(final String tempUrl) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, response -> {
+            StringBuilder output = new StringBuilder();
+
+            try {
+                JSONObject jsonResponse = new JSONObject(response);
+                JSONArray jsonArray = jsonResponse.getJSONArray("data");
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    final JSONObject jsonobject = jsonArray.getJSONObject(i);
+                    double temp = jsonobject.getDouble("temp");
+                    int humidity = jsonobject.getInt("rh");
+                    double wind = jsonobject.getDouble("wind_spd");
+                    String clouds = jsonobject.getString("clouds");
+                    output.append(" Day ").append(i + 1).append("\n").append(df.format(temp)).append(" °F")
+                            .append("\n Humidity: ").append(humidity).append("%").append("\n Wind Speed: ")
+                            .append(df.format(wind)).append(" mph").append("\n Cloudiness: ").append(clouds).append("%\n\n");
+                }
+                daily = output.toString();
+                mBinding.tvResult.setText(daily); // Fill up the textview with the weather data
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> Toast.makeText(getActivity(), error.toString().trim(), Toast.LENGTH_SHORT).show());
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(latLng));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, mMap.getCameraPosition().zoom));
+        coordinates = latLng;
+        getWeatherCurrent("https://api.weatherbit.io/v2.0/current?lat=" + latLng.latitude + "&lon=" + latLng.longitude + "&units=I&key=51500e0f085741f591dc0356d9a03ff4");
+        getWeatherHourly("https://api.weatherbit.io/v2.0/forecast/hourly?lat=" + latLng.latitude + "&lon=" + latLng.longitude + "&hours=24&units=I&key=51500e0f085741f591dc0356d9a03ff4");
+        getWeatherDaily("https://api.weatherbit.io/v2.0/forecast/daily?lat=" + latLng.latitude + "&lon=" + latLng.longitude + "&days=7&units=I&key=51500e0f085741f591dc0356d9a03ff4");
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        LocationViewModel model = new ViewModelProvider(getActivity())
+                .get(LocationViewModel.class);
+        model.addLocationObserver(getViewLifecycleOwner(), location -> {
+            if(location != null) {
+                googleMap.getUiSettings().setZoomControlsEnabled(true);
+                googleMap.setMyLocationEnabled(true);
+
+                if (coordinates == null) {
+                    final LatLng c = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(c, 4.0f));
+                } else {
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 4.0f));
+                }
+
+            }
+        });
+        mMap.setOnMapClickListener(this);
     }
 }
 
