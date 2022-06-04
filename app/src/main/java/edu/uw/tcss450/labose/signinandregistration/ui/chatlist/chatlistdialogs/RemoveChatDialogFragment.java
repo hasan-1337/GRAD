228c3e2de3be51022fbf614af133e924f6d0dafb
naomyte <49845989+naomyte@users.noreply.github.com>
2022-06-04 12:47:44 -0700

    Basics set up for Removing chat.

--- app/src/main/java/edu/uw/tcss450/labose/signinandregistration/ui/chatlist/chatlistdialogs/RemoveChatDialogFragment.java
+++ app/src/main/java/edu/uw/tcss450/labose/signinandregistration/ui/chatlist/chatlistdialogs/RemoveChatDialogFragment.java
@@ -11,42 +11,42 @@ import androidx.lifecycle.ViewModelProvider;
 
 import edu.uw.tcss450.labose.signinandregistration.R;
 import edu.uw.tcss450.labose.signinandregistration.databinding.FragmentCreateChatDialogBinding;
+import edu.uw.tcss450.labose.signinandregistration.databinding.FragmentRemoveChatDialogBinding;
 import edu.uw.tcss450.labose.signinandregistration.model.UserViewModel;
 
 public class RemoveChatDialogFragment extends DialogFragment {
 
-//    public static final String TAG = "CreateChatDialog";
-//    CreateChatDialogViewModel mModel;
-//    UserViewModel mUserModel;
-//    FragmentCreateChatDialogBinding mBinding;
-//
-//    @Override
-//    public Dialog onCreateDialog(Bundle savedInstanceState) {
-//        ViewModelProvider provider = new ViewModelProvider(getActivity());
-//        mUserModel = provider.get(UserViewModel.class);
-//        mModel = provider.get(CreateChatDialogViewModel.class);
-//
-//
-//        // Use the Builder class for convenient dialog construction
-//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//
-//        LayoutInflater inflater = requireActivity().getLayoutInflater();
-//        mBinding = FragmentCreateChatDialogBinding.inflate(inflater);
-//
-//        builder.setView(mBinding.getRoot().getRootView());
-//        builder.setPositiveButton(R.string.confirm_new_chat, new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int id) {
-//                mModel.connectPost(mUserModel.getmJwt(),
-//                        mBinding.newChatName.getText().toString(),
-//                        mBinding.newChatId.getText().toString());
-//            }
-//        });
-//        builder.setNegativeButton(R.string.cancel,
-//                (dialog, id) ->
-//                        CreateChatDialogFragment.this.getDialog().cancel());
-//
-//        return builder.create();
-//    }
+    public static final String TAG = "RemoveChatDialog";
+    RemoveChatDialogViewModel mModel;
+    UserViewModel mUserModel;
+    FragmentRemoveChatDialogBinding mBinding;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        ViewModelProvider provider = new ViewModelProvider(getActivity());
+        mUserModel = provider.get(UserViewModel.class);
+        mModel = provider.get(RemoveChatDialogViewModel.class);
+
+
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        mBinding = FragmentRemoveChatDialogBinding.inflate(inflater);
+
+        builder.setView(mBinding.getRoot().getRootView());
+        builder.setPositiveButton(R.string.confirm_new_chat, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                mModel.connectDelete(mUserModel.getmJwt(),
+                        mBinding.removeChatId.getText().toString());
+            }
+        });
+        builder.setNegativeButton(R.string.cancel,
+                (dialog, id) ->
+                        RemoveChatDialogFragment.this.getDialog().cancel());
+
+        return builder.create();
+    }
 
 }
--- app/src/main/java/edu/uw/tcss450/labose/signinandregistration/ui/chatlist/chatlistdialogs/RemoveChatDialogViewModel.java
+++ app/src/main/java/edu/uw/tcss450/labose/signinandregistration/ui/chatlist/chatlistdialogs/RemoveChatDialogViewModel.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
 import com.android.volley.DefaultRetryPolicy;
@@ -18,57 +19,56 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RemoveChatDialogViewModel {
+public class RemoveChatDialogViewModel extends AndroidViewModel {
 
-//    private MutableLiveData<JSONObject> mResponse;
-//
-//
-//    public CreateChatDialogViewModel(@NonNull Application application) {
-//        super(application);
-//        mResponse = new MutableLiveData<>();
-//        mResponse.setValue(new JSONObject());
-//    }
-//
-//    private void handleError(final VolleyError error) {
-//        Log.e("CONNECTION ERROR", error.getLocalizedMessage());
-//        throw new IllegalStateException(error.getMessage());
-//    }
-//
-//    public void connectPost(final String jwt, final String chatName, final String chatID) {
-//        String url = "https://team-2-tcss450-server-m-c.herokuapp.com/chats";
-//
-//        JSONObject body = new JSONObject();
-//
-//        try {
-//            body.put("name", chatName);
-//            body.put("chatID", Integer.parseInt(chatID));
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-//
-//        Request request = new JsonObjectRequest(
-//                Request.Method.POST,
-//                url,
-//                body,
-//                mResponse::setValue,
-//                this::handleError) {
-//            @Override
-//            public Map<String, String> getHeaders() {
-//                Map<String, String> headers = new HashMap<>();
-//                // add headers <key,value>
-//                headers.put("Authorization", jwt);
-//                return headers;
-//            }
-//        };
-//
-//        request.setRetryPolicy(new DefaultRetryPolicy(
-//                10_000,
-//                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-//                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-//
-//        //Instantiate the RequestQueue and add the request to the queue
-//        Volley.newRequestQueue(getApplication().getApplicationContext())
-//                .add(request);
-//    }
+    private MutableLiveData<JSONObject> mResponse;
+
+
+    public RemoveChatDialogViewModel(@NonNull Application application) {
+        super(application);
+        mResponse = new MutableLiveData<>();
+        mResponse.setValue(new JSONObject());
+    }
+
+    private void handleError(final VolleyError error) {
+        Log.e("CONNECTION ERROR", error.getLocalizedMessage());
+        throw new IllegalStateException(error.getMessage());
+    }
+
+    public void connectDelete(final String jwt, final String chatID) {
+        String url = "https://team-2-tcss450-server-m-c.herokuapp.com/chats" + chatID;
+
+        JSONObject body = new JSONObject();
+
+        try {
+            body.put("chatId", Integer.parseInt(chatID));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Request request = new JsonObjectRequest(
+                Request.Method.DELETE,
+                url,
+                body,
+                mResponse::setValue,
+                this::handleError) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                // add headers <key,value>
+                headers.put("Authorization", jwt);
+                return headers;
+            }
+        };
+
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        //Instantiate the RequestQueue and add the request to the queue
+        Volley.newRequestQueue(getApplication().getApplicationContext())
+                .add(request);
+    }
 
 }
