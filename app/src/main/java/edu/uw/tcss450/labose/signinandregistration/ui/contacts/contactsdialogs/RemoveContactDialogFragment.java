4b1a78c8d7b83af800b62d66d771b8716d8a163c
naomyte <49845989+naomyte@users.noreply.github.com>
2022-06-04 12:43:46 -0700

    Basics set up for Removing contact.

--- app/src/main/java/edu/uw/tcss450/labose/signinandregistration/ui/contacts/contactsdialogs/RemoveContactDialogFragment.java
+++ app/src/main/java/edu/uw/tcss450/labose/signinandregistration/ui/contacts/contactsdialogs/RemoveContactDialogFragment.java
@@ -6,48 +6,46 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import edu.uw.tcss450.labose.signinandregistration.R;
 import edu.uw.tcss450.labose.signinandregistration.databinding.FragmentAddContactDialogBinding;
+import edu.uw.tcss450.labose.signinandregistration.databinding.FragmentRemoveContactDialogBinding;
 import edu.uw.tcss450.labose.signinandregistration.model.UserViewModel;
 
-public class RemoveContactDialogFragment {
-//    public static final String TAG = "AddContactDialog";
-//    AddContactDialogViewModel mModel;
-//    UserViewModel mUserModel;
-//    FragmentAddContactDialogBinding mBinding;
-//
-//    @Override
-//    public Dialog onCreateDialog(Bundle savedInstanceState) {
-//        ViewModelProvider provider = new ViewModelProvider(getActivity());
-//        mUserModel = provider.get(UserViewModel.class);
-//        mModel = provider.get(AddContactDialogViewModel.class);
-//
-//
-//        // Use the Builder class for convenient dialog construction
-//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//
-//        LayoutInflater inflater = requireActivity().getLayoutInflater();
-//        mBinding = FragmentAddContactDialogBinding.inflate(inflater);
-//
-//        builder.setView(mBinding.getRoot().getRootView());
-//        builder.setPositiveButton(R.string.confirm_new_chat, new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int id) {
-//                mModel.connectPost(mUserModel.getmJwt(),
-//                        mBinding.newContactId.getText().toString(),
-//                        mBinding.newContactFirstname.getText().toString(),
-//                        mBinding.newContactLastname.getText().toString(),
-//                        mBinding.newContactEmail.getText().toString(),
-//                        mBinding.newContactUserId.getText().toString());
-//            }
-//        });
-//        builder.setNegativeButton(R.string.cancel,
-//                (dialog, id) ->
-//                        edu.uw.tcss450.labose.signinandregistration.ui.contacts.contactsdialogs
-//                                .AddContactDialogFragment.this.getDialog().cancel());
-//
-//        return builder.create();
-//    }
+public class RemoveContactDialogFragment extends DialogFragment {
+    public static final String TAG = "RemoveContactDialog";
+    RemoveContactDialogViewModel mModel;
+    UserViewModel mUserModel;
+    FragmentRemoveContactDialogBinding mBinding;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        ViewModelProvider provider = new ViewModelProvider(getActivity());
+        mUserModel = provider.get(UserViewModel.class);
+        mModel = provider.get(RemoveContactDialogViewModel.class);
+
+
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        mBinding = FragmentRemoveContactDialogBinding.inflate(inflater);
+
+        builder.setView(mBinding.getRoot().getRootView());
+        builder.setPositiveButton(R.string.confirm_new_chat, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                mModel.connectDelete(mUserModel.getmJwt(),
+                        mBinding.removeContactId.getText().toString());
+            }
+        });
+        builder.setNegativeButton(R.string.cancel,
+                (dialog, id) ->
+                        edu.uw.tcss450.labose.signinandregistration.ui.contacts.contactsdialogs
+                                .RemoveContactDialogFragment.this.getDialog().cancel());
+
+        return builder.create();
+    }
 }
--- app/src/main/java/edu/uw/tcss450/labose/signinandregistration/ui/contacts/contactsdialogs/RemoveContactDialogViewModel.java
+++ app/src/main/java/edu/uw/tcss450/labose/signinandregistration/ui/contacts/contactsdialogs/RemoveContactDialogViewModel.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
 import com.android.volley.DefaultRetryPolicy;
@@ -18,59 +19,53 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RemoveContactDialogViewModel {
-//    private MutableLiveData<JSONObject> mResponse;
-//
-//    public AddContactDialogViewModel(@NonNull Application application) {
-//        super(application);
-//        mResponse = new MutableLiveData<>();
-//        mResponse.setValue(new JSONObject());
-//    }
-//
-//    private void handleError(final VolleyError error) {
-//        Log.e("CONNECTION ERROR", error.getLocalizedMessage());
-//        throw new IllegalStateException(error.getMessage());
-//    }
-//
-//    public void connectPost(final String jwt, final String contactID, final String contactFName,
-//                            final String contactLName, final String contactEmail,
-//                            final String chatID) {
-//        String url = "https://team-2-tcss450-server-m-c.herokuapp.com/contactssql";
-//
-//        JSONObject body = new JSONObject();
-//
-//        try {
-////            body.put("contactid", Integer.parseInt(contactID));
-//            body.put("FirstName", contactFName);
-//            body.put("LastName", contactLName);
-//            body.put("Email", contactEmail);
-//            body.put("ChatId", Integer.parseInt(chatID));
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-//
-//        Request request = new JsonObjectRequest(
-//                Request.Method.POST,
-//                url,
-//                body,
-//                mResponse::setValue,
-//                this::handleError) {
-//            @Override
-//            public Map<String, String> getHeaders() {
-//                Map<String, String> headers = new HashMap<>();
-//                // add headers <key,value>
-//                headers.put("Authorization", jwt);
-//                return headers;
-//            }
-//        };
-//
-//        request.setRetryPolicy(new DefaultRetryPolicy(
-//                10_000,
-//                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-//                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-//
-//        //Instantiate the RequestQueue and add the request to the queue
-//        Volley.newRequestQueue(getApplication().getApplicationContext())
-//                .add(request);
-//    }
+public class RemoveContactDialogViewModel extends AndroidViewModel {
+    private MutableLiveData<JSONObject> mResponse;
+
+    public RemoveContactDialogViewModel(@NonNull Application application) {
+        super(application);
+        mResponse = new MutableLiveData<>();
+        mResponse.setValue(new JSONObject());
+    }
+
+    private void handleError(final VolleyError error) {
+        Log.e("CONNECTION ERROR", error.getLocalizedMessage());
+        throw new IllegalStateException(error.getMessage());
+    }
+
+    public void connectDelete(final String jwt, final String contactID) {
+        String url = "https://team-2-tcss450-server-m-c.herokuapp.com/contactssql";
+
+        JSONObject body = new JSONObject();
+
+        try {
+            body.put("contactId", Integer.parseInt(contactID));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Request request = new JsonObjectRequest(
+                Request.Method.DELETE,
+                url,
+                body,
+                mResponse::setValue,
+                this::handleError) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                // add headers <key,value>
+                headers.put("Authorization", jwt);
+                return headers;
+            }
+        };
+
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        //Instantiate the RequestQueue and add the request to the queue
+        Volley.newRequestQueue(getApplication().getApplicationContext())
+                .add(request);
+    }
 }
